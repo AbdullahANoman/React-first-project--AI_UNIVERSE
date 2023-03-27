f10f8b74f0cc81817ab84a3ba61edaf623661ac2
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-03-27 17:13:43 +0600

    Added ternary operator

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Modal = (props) => {
     // console.log(props.singleModal)
-    const {description,pricing,image_link} = props.singleModal;
+    const {description,pricing,image_link,integrations,features} = props.singleModal;
     // console.log(pricing)
+    const featuresAll = Object.values(features|| {})
     return (
         <div className=''>
            <input type="checkbox" id="my-modal-5" className="modal-toggle" />
@@ -15,15 +16,29 @@ const Modal = (props) => {
                     <p>{description}</p>
                 </div>
                 <div className='flex gap-4 '>
-                  {/* {
-                    pricing.map(price=>{
+                  {
+                   pricing && pricing.map(price=>{
                       return (
                         <div className='bg-white p-5 border-2 rounded-lg'>
-                          <p>{price.price} <br /> {price.plan}</p>
+                          <p>{price?price.price : 'Not Found'} <br /> {price?price.plan:'Not Found'}</p>
                         </div>
                       )
                     })
-                  } */}
+                  }
+                </div>
+                <div className='flex gap-3'>
+                  <div>
+                    <h1 className='text-2xl font-bold ml-2 mt-5'>Feature</h1>
+                    { 
+                     featuresAll &&  featuresAll.map(feature=><li className='pl-4'>{feature?feature.feature_name:'Not Found'}</li>)
+                    }
+                  </div>
+                  <div>
+                  <h1 className='text-2xl font-bold ml-2 mt-5'>Integrations</h1>
+                  {
+                   integrations && integrations.map(item=><li>{item?item:"Not Found"}</li>)
+                  }
+                  </div>
                 </div>
                 </div>
                 <div className='w-full'>
