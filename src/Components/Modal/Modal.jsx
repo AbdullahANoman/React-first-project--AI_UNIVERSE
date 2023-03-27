bcfaa95909b3d0899e41f67a8dc559875e3a76d4
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-03-27 16:28:30 +0600

    Push with Modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -2,15 +2,34 @@ import React from 'react';
 
 const Modal = (props) => {
     // console.log(props.singleModal)
-    const {id,description} = props.singleModal
+    const {description,pricing,image_link} = props.singleModal;
+    // console.log(pricing)
     return (
-        <div>
+        <div className=''>
            <input type="checkbox" id="my-modal-5" className="modal-toggle" />
-        <div className="modal">
-          <div className="modal-box w-11/12 max-w-5xl">
-            <div>
+        <div className="modal ">
+          <div className="modal-box w-11/12 max-w-5xl ">
+            <div className='flex gap-5'>
+                <div className='bg-red-100 w-full'>
                 <div>
                     <p>{description}</p>
+                </div>
+                <div className='flex gap-4 '>
+                  {/* {
+                    pricing.map(price=>{
+                      return (
+                        <div className='bg-white p-5 border-2 rounded-lg'>
+                          <p>{price.price} <br /> {price.plan}</p>
+                        </div>
+                      )
+                    })
+                  } */}
+                </div>
+                </div>
+                <div className='w-full'>
+                  <div>
+                    <img className='w-96 h-80' src={image_link ? image_link[0]:null} alt="" />
+                  </div>
                 </div>
             </div>
             <div className="modal-action">
